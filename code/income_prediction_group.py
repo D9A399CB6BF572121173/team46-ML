b29785f3a4dc66f0f3b4941575c5f7ddb41e94db
Mark Anderson <andersm3@tcd.ie>
2019-11-21 19:42:08 +0000

    extra pre-processing/comments

--- code/income_prediction_group.py
+++ code/income_prediction_group.py
@@ -11,22 +11,42 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error
 
+# Read in CSV files and put into dataframes
 dataset_training = pd.read_csv('../data/tcd-ml-1920-group-income-train.csv', low_memory=False)
 dataset_predict = pd.read_csv('../data/tcd-ml-1920-group-income-test.csv', low_memory=False)
 
-numerical_features = ['Year of Record', 'Work Experience in Current Job [years]', 'Age',
-        'Size of City']
+##### Pre-processing code starts here #####
+
+# define which features are numerical, categorical or constructed
+numerical_features = ['Year of Record', 'Work Experience in Current Job [years]',
+        'Size of City', 'Yearly Income in addition to Salary (e.g. Rental Income)']
 categorical_features = ['Housing Situation', 'Satisfation with employer', 'Gender', 'Country',
         'Hair Color']
-constructed_categorical_features = ['Body Height [cm]', 'Crime Level in the City of Employement']
+constructed_categorical_features = ['Body Height [cm]', 'Crime Level in the City of Employement', 'Age']
 
-for feature in constructed_categorical_features:
-    dataset_training[feature] = pd.cut(dataset_training[feature], 3, labels=['Below','Average','Above'])
+# bin intervals for constructed categories
+height_bins = pd.IntervalIndex.from_tuples([(0, 155), (156, 195), (196, 300)])
+crime_bins = pd.IntervalIndex.from_tuples([(0, 33), (33, 133), (133, 200)])
+age_bins = pd.IntervalIndex.from_tuples([(0, 9), (10, 19), (20, 29), (30, 39), (40, 49), (50, 59), (60, 69), (70, 79), (80, 89), (90, 99), (100, 109), (110, 119), (120, 129)])
 
+# Apply bins to data
+dataset_training['Body Height [cm]'] = pd.cut(dataset_training['Body height [cm]'], height_bins, labels=['Below','Average','Above'])
+dataset_training['Crime Level in the City of Employement'] = pd.cut(dataset_training['Body Height [cm]'], crime_bins, labels=['Below', 'Average', 'Above'])
+dataset_training['Age' = pd.cut(dataset_training['Age'], age_bins, labels=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'])
+
+dataset_predict['Body Height [cm]'] = pd.cut(dataset_predict['Body height [cm]'], height_bins, labels=['Below','Average','Above'])
+dataset_predict['Crime Level in the City of Employement'] = pd.cut(dataset_predict['Body Height [cm]'], crime_bins, labels=['Below', 'Average', 'Above'])
+dataset_predict['Age'] = pd.cut(dataset_predict['Age'], age_bins, labels=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'])
+
+# Remove some data that contains strings instead of floats (not so elegant)
 dataset_training = dataset_training[~dataset_training['Work Experience in Current Job [years]'].str.contains('#NUM!')]
 
+# additional_income_training = dataset_training['Yearly Income in addition to Salary (e.g. Rental Income)'].to_numpy()
+# additional_income_predict = dataset_prediction['Yearly Income in addition to Salary (e.g. Rental Income)'].to_numpy()
+
+# Data pipelines, better to substitute with individual preprocessing steps perhaps
 numerical_transformer = Pipeline(steps=[
-    ('Imputer', SimpleImputer(strategy='median', verbose=1)),
+    ('Imputer', SimpleImputer(strategy='mode', verbose=1)),
     ('Scaler', StandardScaler())],
     verbose=True)
 
@@ -42,22 +62,31 @@ preprocessor = ColumnTransformer(
             ('Binned Data', categorical_transformer, constructed_categorical_features),],
         verbose=True)
 
+##### Pre-processing code ends here #####
+
+# system pipeline
 regressor = Pipeline(steps=[
     ('Preprocessor', preprocessor),
     ('XGBoost', xgb.XGBRegressor())],
     verbose=True)
 
-x_data = dataset_training.drop(['Instance', 'Yearly Income in addition to Salary (e.g. Rental Income)',
-    'Total Yearly Income [EUR]'], axis=1)
+# Create features and target vectors
+x_data = dataset_training.drop(['Instance', 'Total Yearly Income [EUR]'], axis=1)
 y_data = dataset_training['Total Yearly Income [EUR]']
 
+x_pred = dataset_predict.drop(['Instance', 'Total Yearly Income [EUR]'], axis=1)
+
+# validation/train split
 x_train, x_test, y_train, y_real = train_test_split(x_data, y_data, test_size=0.3, random_state=0)
 
 model = regressor.fit(x_train, y_train)
-y_pred = model.predict(x_test)
 
-predictions.pd.DataFrame(data=regressor.predict(y_pred), columns='Total Yearly Income [EUR]')
-predictions.insert(0, 'Instance', range(1, len(df)))
+y_test = model.predict(x_test)
+y_pred = model.predict(x_pred)
+
+# write to csv, can improve so it writes out submittable file
+predictions = pd.DataFrame(y_pred)
 predictions.to_csv('../pred.csv')
 
-print('MAE: ', mean_absolute_error(y_real, y_pred))
+# Print MAE
+print('MAE: ', mean_absolute_error(y_test, y_real)
